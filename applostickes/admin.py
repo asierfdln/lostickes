d00da355e8a2a9da281d47d7e019d54caea7322c
unknown <asierfdln@gmail.com>
2020-11-26 17:19:21 +0100

    Admin bien y vuelta a models

--- applostickes/admin.py
+++ applostickes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Departamento, Habilidad, Empleado
+
+admin.site.register(Departamento)
+admin.site.register(Habilidad)
+admin.site.register(Empleado)
