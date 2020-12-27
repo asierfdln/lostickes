bbc8619f518158dcf9690d76fdf9baca2290ac4a
unknown <asierfdln@gmail.com>
2020-12-27 12:09:23 +0100

    htmls hechos

--- applostickes/static/applostickes/showhideelems.js
+++ applostickes/static/applostickes/showhideelems.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
         if ($(this).prop('innerText') == 'Hide old elements') {
             $(this).prop('innerText', 'Show old elements');
             // hide stuff
-            $('#ultohide').prev().prev().hide();
-            $('#ultohide').prev().hide();
+            // $('#ultohide').prev().prev().hide();
+            // $('#ultohide').prev().hide();
             $('#ultohide').hide();
         } else {
             $(this).prop('innerText', 'Hide old elements');
             // show stuff
-            $('#ultohide').prev().prev().show();
-            $('#ultohide').prev().show();
+            // $('#ultohide').prev().prev().show();
+            // $('#ultohide').prev().show();
             $('#ultohide').show();
         }
     });
